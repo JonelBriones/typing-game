056e5fac52de4a858ea6b3dfadcf7704bee3b56c
JonelBriones <jonel.c.briones@gmail.com>
2025-03-09 22:15:12 -0700

    export default app

--- server/server.js
+++ server/server.js
@@ -33,4 +33,4 @@ if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log("Server running on:", PORT));
 }
-export default { app };
+export default app;
