560b6969a4a5eabae3fc81defcddd89ef4a74e30
JonelBriones <jonel.c.briones@gmail.com>
2025-03-09 22:10:16 -0700

    testing, basic server file

--- server/server.js
+++ server/server.js
@@ -12,23 +12,22 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Backend for CloudyType is working" });
 });
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    "https://cloudytype.vercel.app",
-    "https://cloudytype-git-typeboard-validations-jonels-projects-684e0f31.vercel.app",
-  ],
-  credentials: true,
-};
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors(corsOptions));
-connectDB(process.env.MONGODB_URI);
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "http://localhost:4173",
+//     "https://cloudytype.vercel.app",
+//     "https://cloudytype-git-typeboard-validations-jonels-projects-684e0f31.vercel.app",
+//   ],
+//   credentials: true,
+// };
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors(corsOptions));
+// connectDB(process.env.MONGODB_URI);
 
-app.use(express.static("dist"));
-app.use("/api/user", userRoute);
-app.use("/api/tests", testRoute);
+// app.use("/api/user", userRoute);
+// app.use("/api/tests", testRoute);
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
